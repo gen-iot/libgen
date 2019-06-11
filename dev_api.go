947aae15ce8470475fbb2b2a2bbfae1148fb74c7
XuChao <xuchao_1213@163.com>
2019-06-11 15:35:25 +0800

    dev_api add some device model related api

--- dev_api.go
+++ dev_api.go
@@ -6,15 +6,47 @@ func Init() {
 	fmt.Println("LIBGEN INIT")
 }
 
-func AddDevice() error {
+//declare device models,only device model declared can be used in device
+func DeclareDeviceModel(models ...DeviceModel) error {
+
 	return nil
 }
 
+//remove device models
+func RemoveDeviceModels(modIds ...string) error {
+
+	return nil
+}
+
+//update  device model
+func UpdateDeviceModel(modId string, model DeviceModel) error {
+
+	return nil
+}
+
+//register devices,
+//each device's modelId must be filled with device model declared ahead
+func RegisterDevices(devices ...Device) error {
+	return nil
+}
+
+//remove devices
 func RemoveDevices(devIds ...string) error {
 	return nil
 }
 
+//update device room
+func UpdateDeviceRoom(devId string, newRoom string) error {
+	return nil
+}
+
+//update device name
 func UpdateDeviceName(devId string, newName string) error {
 	return nil
 }
 
+//update device status properties,
+//statusProps must contains all status properties defined in DeviceModel
+func UpdateDeviceStatus(devId string, statusProps map[string]interface{}) error {
+	return nil
+}
