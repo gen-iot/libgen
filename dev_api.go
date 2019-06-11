e8c441c5d4df1c038d9a57c5e8f571a54019f485
XuChao <xuchao_1213@163.com>
2019-06-11 15:47:57 +0800

    dev_api add some api

--- dev_api.go
+++ dev_api.go
@@ -7,18 +7,21 @@ func Init() {
 }
 
 //declare device models,only device model declared can be used in device
+//ps:this function need some privilege//TODO define privilege
 func DeclareDeviceModel(models ...DeviceModel) error {
 
 	return nil
 }
 
 //remove device models
+//ps:this function need some privilege//TODO define privilege
 func RemoveDeviceModels(modIds ...string) error {
 
 	return nil
 }
 
 //update  device model
+//ps:this function need some privilege//TODO define privilege
 func UpdateDeviceModel(modId string, model DeviceModel) error {
 
 	return nil
@@ -26,27 +29,54 @@ func UpdateDeviceModel(modId string, model DeviceModel) error {
 
 //register devices,
 //each device's modelId must be filled with device model declared ahead
+//ps:this function need some privilege//TODO define privilege
 func RegisterDevices(devices ...Device) error {
 	return nil
 }
 
 //remove devices
+//ps:this function need some privilege//TODO define privilege
 func RemoveDevices(devIds ...string) error {
 	return nil
 }
 
 //update device room
+//ps:this function need some privilege//TODO define privilege
 func UpdateDeviceRoom(devId string, newRoom string) error {
 	return nil
 }
 
 //update device name
+//ps:this function need some privilege//TODO define privilege
 func UpdateDeviceName(devId string, newName string) error {
 	return nil
 }
 
 //update device status properties,
 //statusProps must contains all status properties defined in DeviceModel
+//ps:this function need some privilege//TODO define privilege
 func UpdateDeviceStatus(devId string, statusProps map[string]interface{}) error {
+	return nil
+}
+
+//device control callback function,
+//this function will be called while someone want control device on your domain
+//ps:this function need some privilege//TODO define privilege
+func OnDeviceControl(devId string, cmdProps map[string]interface{}) error {
+
+	return nil
+}
+
+//control device,control some device in other domain
+//ps:this function need some privilege//TODO define privilege
+func DeviceControl(domain string, deviceId string, cmdProps map[string]interface{}) error {
+
+	return nil
+}
+//this is a notify callback function,
+//this function will be called while devices'status which in other domain changed
+//ps:this function need some privilege//TODO define privilege
+func OnDeviceStatusChanged(domain string, deviceId string, statusProps map[string]interface{}) error {
+
 	return nil
 }
