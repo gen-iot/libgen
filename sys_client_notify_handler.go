d50253e193fcf62bb18f85d9dcfbbaff2c5740ab
XuChao <xuchao_1213@163.com>
2019-07-15 13:53:10 +0800

    fix : sys_client rpc func in param[0] change to rpcx.Context

--- sys_client_notify_handler.go
+++ sys_client_notify_handler.go
@@ -13,21 +13,21 @@ type DeviceStatusHandler func(notify *DeviceStatusNotify)
 var gDeviceControlHandler DeviceControlHandler
 var gDeviceStatusHandler DeviceStatusHandler
 
-func pong(callable rpcx.Callable, req *Ping) (*Pong, error) {
+func pong(ctx rpcx.Context, req *Ping) (*Pong, error) {
 	log.Println("receive ping req.time =", req.Time, " delta is ", time.Now().Sub(req.Time))
 	return &Pong{Time: time.Now(), Msg: "client pong"}, nil
 }
 
 var errAppNotImpControl = errors.New("app not support control device yet")
 
-func onDeviceControl(callable rpcx.Callable, req *ControlDeviceRequest) (*ControlDeviceResponse, error) {
+func onDeviceControl(ctx rpcx.Context, req *ControlDeviceRequest) (*ControlDeviceResponse, error) {
 	if gDeviceControlHandler != nil {
 		return gDeviceControlHandler(req)
 	}
 	return nil, errAppNotImpControl
 }
 
-func onDeviceStatusDelivery(callable rpcx.Callable, notify *DeviceStatusNotify) (*BaseResponse, error) {
+func onDeviceStatusDelivery(ctx rpcx.Context, notify *DeviceStatusNotify) (*BaseResponse, error) {
 	if gDeviceStatusHandler != nil {
 		go gDeviceStatusHandler(notify)
 	}
