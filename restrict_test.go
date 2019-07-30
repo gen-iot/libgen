7c55f5364704c108e4d23510e50a7bac79607a87
Suzhen <suzhen@homeinpocket.net>
2019-07-30 17:40:35 +0800

    :white_check_mark: test for restrict

---
+++ restrict_test.go
@@ -0,0 +1,23 @@
+package libgen
+
+import (
+	"github.com/gen-iot/std"
+	"testing"
+)
+
+func must(err error) {
+	std.AssertError(err, "failed")
+}
+
+func mustErr(err error) {
+	std.Assert(err != nil, "failed")
+}
+
+func TestI32EnumLimiter(t *testing.T) {
+	limiter := NewI32EnumLimiter(1, 2, 3, 4, 5)
+	must(limiter.Validate(1))
+	must(limiter.Validate(2))
+	must(limiter.Validate(3))
+	mustErr(limiter.Validate(40))
+	mustErr(limiter.Validate(50))
+}
