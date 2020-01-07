4cae168d56913d439c69553069bd2ce567b85bd6
Suzhen <suzhen@homeinpocket.net>
2020-01-07 11:24:12 +0800

    :sparkles: array limiter add Min/Max length, and not empty

--- rstrict_array.go
+++ rstrict_array.go
@@ -7,18 +7,32 @@ import (
 type ArrayLimiter struct {
 	baseRestrict
 	ElementRestrict Restrict `json:"elementRestrict" validate:"required"`
-	FixedLength     int      `json:"fixedLength"`
+	MinLength       int      `json:"minLength"`
+	MaxLength       int      `json:"maxLength"`
+	NotEmpty        bool     `json:"notEmpty"`
 }
 
 func NewArrayLimiter(name string, elementValidator Restrict) Restrict {
 	out := new(ArrayLimiter)
 	out.ElementRestrict = elementValidator
-	out.FixedLength = -1
+	out.MinLength = -1
+	out.MaxLength = -1
+	out.NotEmpty = false
 	if out.ElementRestrict == nil {
 		out.ElementRestrict = NewAnyLimiter("validator", false)
 	}
 	out.baseRestrict = newBaseRestrict(out, name, Array)
 	return out.SetDefaultValue(true, []interface{}{})
+}
+
+func (this *ArrayLimiter) SetMinLength(minLen int) *ArrayLimiter {
+	this.MinLength = minLen
+	return this
+}
+
+func (this *ArrayLimiter) SetMaxLength(maxLen int) *ArrayLimiter {
+	this.MaxLength = maxLen
+	return this
 }
 
 func (this *ArrayLimiter) Validate(v interface{}) error {
@@ -29,8 +43,14 @@ func (this *ArrayLimiter) Validate(v interface{}) error {
 	if !ok {
 		return errIllegalParams
 	}
-	if this.FixedLength != -1 && len(va) != this.FixedLength {
-		return errors.Wrapf(errArrayLengthMismatched, "expect=%d, real=%d", this.FixedLength, len(va))
+	if this.NotEmpty && len(va) == 0 {
+		return errArrayEmpty
+	}
+	if this.MinLength != -1 && len(va) < this.MinLength {
+		return errors.Wrapf(errArrayLengthMismatched, "expect >= %d, real=%d", this.MinLength, len(va))
+	}
+	if this.MaxLength != -1 && len(va) > this.MaxLength {
+		return errors.Wrapf(errArrayLengthMismatched, "expect <= %d, real=%d", this.MaxLength, len(va))
 	}
 	for _, it := range va {
 		if err := this.ElementRestrict.Validate(it); err != nil {
